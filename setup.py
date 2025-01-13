208ae529fb1d9d3fa039e202b77d45de2e51eddd
NonoInge <nurnoemie@gmail.com>
2025-01-13 14:03:13 +0100

    Adding of setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+name='QuizGame',
+version='1.0',
+author='Noëmie Nurijanyan',
+license='MIT',
+long_description=open('README.md').read(),
+)
